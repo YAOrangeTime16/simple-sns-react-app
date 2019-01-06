e668d8dd0b8cbb43d5abd8f716855cb986b17b42
Yoko Andrae <yoko@enliven.co>
2019-01-06 14:31:20 +0100

    Updates

---
+++ precache-manifest.c8196aa8409d891251decb1640bf5871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a63c74976d4a5cda458",
+    "url": "/simple-sns-react-app/static/css/main.99ffc54a.chunk.css"
+  },
+  {
+    "revision": "6a63c74976d4a5cda458",
+    "url": "/simple-sns-react-app/static/js/main.6a63c749.chunk.js"
+  },
+  {
+    "revision": "cf04122136cacf312f2c",
+    "url": "/simple-sns-react-app/static/js/1.cf041221.chunk.js"
+  },
+  {
+    "revision": "ed0e386203c6ee71c188",
+    "url": "/simple-sns-react-app/static/js/runtime~main.ed0e3862.js"
+  },
+  {
+    "revision": "26ed1174e339bbb0edc80ada4ba8cebd",
+    "url": "/simple-sns-react-app/index.html"
+  }
+];
